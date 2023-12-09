59b3227e78caf88dd70eeac4161869764a3b2551
dangannn <dn14072004@mail.ru>
2023-12-09 11:52:26 +0300

    feat: validation

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from projects.models import Project, ProjectRequest
@@ -7,6 +8,14 @@ class ProjectSerializer(ModelSerializer):
     class Meta:
         model = Project
         fields = ['id', 'name', 'description', 'urgency', 'payment', 'customer', 'freelancer', 'pub_date']
+
+    def validate_payment(self, value):
+        """
+        Проверка валидности поля "payment".
+        """
+        if value < 0:
+            raise serializers.ValidationError("Оплата не может быть отрицательной.")
+        return value
 
 
 class ProjectRequestSerializer(ModelSerializer):
